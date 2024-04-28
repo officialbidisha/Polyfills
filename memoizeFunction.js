718ca3cb56c412c1a9383062445efa5a9b7fb79b
Bidisha Das <officialbidisha1@gmail.com>
2024-04-28 12:46:22 +0530

    Create memoizeFunction.js

---
+++ memoizeFunction.js
@@ -0,0 +1,56 @@
+function memo(func, resolver) {
+  // your code here
+  const cache = new Map();
+
+  // Map<cacheKey, Map<context, value>>
+  return function () {
+    const cacheKey = resolver
+      ? resolver(...arguments)
+      : Array.from(arguments).join(",");
+    console.log("exp", new Map([[1, 2]]));
+    const contextMap = cache.get(cacheKey);
+    // If there is a corresponding context map to cachekey
+    // Check if context is in the map, if so, return value.
+    // Else if no corresponding add contextMap, add new entry to the context map
+    if (!contextMap) {
+      const value = func.apply(this, arguments);
+      let z = new Map();
+      console.log(z.set(this, value));
+      console.log("x", new Map([[this, value]]));
+
+      console.log("ew", cache.get(cacheKey));
+      return value;
+    }
+
+    if (contextMap.has(this)) {
+      return contextMap.get(this);
+    }
+    // If context not in the map, calculate and add to context map.
+    const value = func.apply(this, arguments);
+    contextMap.set(this, value);
+    return value;
+  };
+}
+
+function testThis(a) {
+  return `${this.val}_${a}`;
+}
+
+const memoFunc = memo(testThis);
+
+const testSubject = {
+  val: 1,
+  memo: memoFunc,
+};
+
+const testSubject2 = {
+  val: 2,
+  memo: memoFunc,
+};
+
+// 1_1
+console.log(testSubject.memo(1));
+// Expected no caching and output is 2_1
+console.log(testSubject2.memo(1));
+// Expected to cache
+console.log(testSubject2.memo(1));
