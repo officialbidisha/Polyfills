a94e3490046126fda16332190c750904c2caeaf5
Bidisha Das <officialbidisha1@gmail.com>
2024-04-28 12:38:29 +0530

    Update dependencyGraph.js

--- dependencyGraph.js
+++ dependencyGraph.js
@@ -101,90 +101,142 @@ console.log(executeTasks(schedules));
 -----------------------------------------------------------------------------------------------------------------------------------------
 
 // Approach 2
-
 class Task {
-    dependencies;
-    job;
-    currentDependencyCount;
-    isCompleted;
-    subscribedList;
+  // accept the dependencies list
+  // and the callback
+  constructor(dependencies = [], job) {
+    debugger;
+    // filter the dependencies that are not yet completed
+    this.dependencies = dependencies
+      ? dependencies.filter(
+          (dependency) => dependency instanceof Task && !dependency.isCompleted
+        )
+      : [];
+    this.currentDependencyCount = this.dependencies.length;
 
-    constructor(dependencies = [], job) {
-        this.dependencies = dependencies ? dependencies.filter(dependency => dependency instanceof Task && !dependency.isCompleted) : [];
-        this.currentDependencyCount = this.dependencies.length;
-        this.job = job;
-        this.isCompleted = false;
-        this.processJob();
-        this.subscribedList = [];
-    }
+    // the callback
+    this.job = job;
 
-    processJob() {
-        if (this.dependencies && this.dependencies.length) {
-            for (let dependency of this.dependencies) {
-                dependency.subscribe(this.trackDependency.bind(this));
-            }
-        } else {
-            this.job(this.done.bind(this));
-        }
-    }
+    // if current task is done
+    this.isCompleted = false;
 
-    trackDependency() {
-        this.currentDependencyCount--;
-        if (this.currentDependencyCount === 0) {
-            this.job(this.done.bind(this));
-        }
-    }
+    // store the dependencies list callback
+    // to execute is sequence
+    this.subscribedList = [];
 
-    subscribe(cb) {
-        this.subscribedList.push(cb);
-    }
+    // start the job
+    this.processJob();
+  }
 
-    done() {
-        this.isCompleted = true;
-        for (const callback of this.subscribedList) {
-            callback();
-        }
+  processJob() {
+    // if there is dependency
+    // subsribe to each of them
+    if (this.dependencies && this.dependencies.length) {
+      for (let dependency of this.dependencies) {
+        dependency.subscribe(this.trackDependency.bind(this));
+        /**
+         * Let's say A is dependent on C
+         * So during the execution of A, inside C's subscription SubscribedList
+         * The trackDependencyof A is pushed.
+         * So when C finished execution, during done,
+         * the trackDependency of A is called
+         * What does track dependency do? Reduces the count of dependent
+         * As C is already executed
+         * If A has no other dependency, A is executed
+         */
+      }
     }
+    // else invoke the callback directly
+    else {
+      /**
+       * Callback function
+       */
+      this.job(this.done.bind(this));
+    }
+  }
+
+  // if all the dependecies are excuted
+  // invoke the callback
+  trackDependency() {
+    this.currentDependencyCount--;
+    if (this.currentDependencyCount === 0) {
+      /**
+       * This is expected as the callback takes done as a parameter
+       */
+      this.job(this.done.bind(this));
+    }
+  }
+
+  // push the callback to the list
+  subscribe(cb) {
+    /**
+     * This subscribedList stores the trackDependency of dependency
+     * It is filled when the task has a dependency
+     * Initially a task, at the time of construction has no dependency
+     * The moment processJOb function is executed, a furthur fucntion
+     * will populate subscribedList
+     *
+     * The subscribedList has the trackDependency function
+     * When the trackDependency is invoked? - in done
+     *
+     * Then it marks the isCompleted for itself as true
+     *          It is simply invoke trackDependency function as
+     */
+    /**
+     * C->A
+     *
+     */
+    this.subscribedList.push(cb);
+  }
+
+  // if the current task is done
+  // mark it as complete
+  // invoke all the dependecy callbacks.
+  // to print it in sequence
+  done() {
+    this.isCompleted = true;
+    /**
+     * SubscribedList: If a task
+     */
+    // console.log(JSON.stringify(this.subscribedList));
+    for (const callback of this.subscribedList) {
+      callback();
+    }
+  }
 }
 
-const processA = new Task(null, (done) => {
-    setTimeout(() => {
-        console.log('Process A');
-        done();
-    }, 100);
-});
-
-const processB = new Task(null, (done) => {
-    setTimeout(() => {
-        console.log('Process B');
-        done();
-    }, 1500);
-});
-
+/**
+ * Take a parameter in
+ */
 const processC = new Task(null, (done) => {
-    setTimeout(() => {
-        console.log('Process C');
-        done();
-    }, 1000);
-});
-
-const processD = new Task([processA, processB], (done) => {
-    setTimeout(() => {
-        console.log('Process D');
-        done();
-    }, 1000);
-})
-
-const processE = new Task([processC, processD], (done) => {
-    setTimeout(() => {
-        console.log('Process E');
-        done();
-    }, 100);
-});
-
-const createAllDoneInstance = (allDoneCallback) => new Task([processA, processB, processC, processD, processE], allDoneCallback);
-
-createAllDoneInstance((done) => {
-    console.log('All is done!');
+  setTimeout(() => {
+    console.log("Process C");
     done();
+  }, 1000);
 });
+/**
+ * A dependent task that has a very high duration
+ * Even if it has dependenies on few other task.
+ * It has a dependency, it has already resolved.
+ */
+const processB = new Task([processC], (done) => {
+  setTimeout(() => {
+    console.log("Process B");
+    done();
+  }, 1500);
+});
+const processA = new Task([processB], (done) => {
+  setTimeout(() => {
+    console.log("Process A");
+    done();
+  }, 100);
+});
+
+// Output:
+// "Process A"
+// "Process C"
+// "Process B"
+// "Process D"
+// "Process E"
+// "All is done!"
+
