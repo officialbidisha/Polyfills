0dcbe48362c56048f751438afdc35145669c2eb5
Bidisha Das <officialbidisha1@gmail.com>
2024-05-19 15:04:01 +0530

    Create async_memoize.js

---
+++ async_memoize.js
@@ -0,0 +1,45 @@
+/**
+ * Memoize the below function
+ */
+function getSomeData(foo, callback) {
+  console.log("ASYNC REQ CALLED");
+  let uri = "https://jsonplaceholder.typicode.com/todos/" + foo;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("async request");
+      fetch(uri).then((res) => {
+        resolve(res);
+      });
+    }, 300);
+  });
+}
+//
+function memoize(fn) {
+  const cache = {};
+
+  return async function () {
+    const args = JSON.stringify(arguments);
+    cache[args] = cache[args] || fn.apply(this, arguments);
+    return cache[args];
+  };
+}
+
+const mem = memoize(getSomeData);
+let cb = (err, res) => {
+  if (err) {
+    console.log("err", err);
+    return;
+  } else {
+    console.log("res", res);
+  }
+};
+
+const x = mem(2, cb);
+x.then((res) => {
+  return res.clone().json();
+}).then((r) => console.log("A", r));
+
+const y = mem(2, cb);
+y.then((res) => {
+  return res.clone().json();
+}).then((r) => console.log("B", r));
