f3072cc07e68de4bb59dc5e3cf68211e3024bb5a
Bidisha Das <officialbidisha1@gmail.com>
2024-08-05 14:11:28 +0530

    Update completeAssign.js

--- completeAssign.js
+++ completeAssign.js
@@ -1,1 +1,22 @@
 // 
+function completeAssign(target, ...sources) {
+  // test case: completeAssign should throw error when target is null or undefined 
+  if(target === null || target === undefined){
+    throw Error("Target is not defined");
+  }
+  if(typeof target !== 'object'){
+    return Object(target);
+  }
+  for(const source of sources){
+    //test case: non-string primitives in source are ignored
+    if(source === null || source === undefined){
+      continue;
+    }
+    // Shorthand way to clone an object with descriptors.
+    // Source: https://javascript.info/property-descriptors#object-getownpropertydescriptors
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/defineProperties
+    console.log(Object.getOwnPropertyDescriptors(source))
+    Object.defineProperties(target, Object.getOwnPropertyDescriptors(source));
+  }
+  return target;
+}
