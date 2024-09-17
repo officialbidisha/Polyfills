09e8893f50c3782d31ea0068c375a2be6e3aa7c5
Bidisha Das <officialbidisha1@gmail.com>
2024-09-17 11:48:40 +0530

    Create AutoRetryPromiseWithRejection.js

---
+++ AutoRetryPromiseWithRejection.js
@@ -0,0 +1,20 @@
+function fetchWithAutoRetry(fetcher, maximumRetryCount) {
+  // Wrap in a Promise so we can resolve/reject properly
+  return new Promise((resolve, reject) => {
+    function tryFetched(remainingRetries) {
+      // Call the fetcher (if it's a function that returns a promise)
+      fetcher().then((res) => {
+        resolve(res);
+      }).catch((err) => {
+        if (remainingRetries > 0) {
+          tryFetched(remainingRetries - 1); // Retry with decremented count
+        } else {
+          reject(err); // No retries left, reject with the error
+        }
+      });
+    }
+    
+    // Start the initial fetch attempt
+    tryFetched(maximumRetryCount);
+  });
+}
