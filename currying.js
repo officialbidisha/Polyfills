b56d7623718e780c7d2e45ea27060b2d9df36536
Bidisha Das <officialbidisha1@gmail.com>
2024-09-22 15:16:56 +0530

    Update currying.js

--- currying.js
+++ currying.js
@@ -1,37 +1,3 @@
-
-// empty args
-const sum = (...args) => {
-  //spread the arguments in storage array
-  const storage = [...args];
-  //base case
-  //if invoked without any argument
-  if(storage.length === 0){
-    return 0;
-  }
-  //closure
-  else{
-    //create an inner function
-    const temp = function(...args2){
-      //get the arguments of inner function
-      //merge them in existing storage
-      storage.push(...args2);
-
-      //if no arguments are passed 
-      //return the value
-      if(args2.length === 0){
-        return storage.reduce((a, b) => a + b, 0);
-      }
-      //else return the same function again
-      else{
-        return temp;
-      }
-    }
-    //return the function
-    return temp;
-  }
-}
-
-
 // 4 args
 const sum = (...args) => {
   //spread the arguments in storage array
