0eb2660d7619d78a3c0aedf0b355ddf80d28b3b3
Bidisha Das <officialbidisha1@gmail.com>
2024-09-23 07:00:48 +0530

    Update instanceOf.js

--- instanceOf.js
+++ instanceOf.js
@@ -1,14 +1,10 @@
 function myInstanceOf(obj, target) {
   // your code here
-  let flag = false;
+  // let flag = false;
    if(obj === null || typeof obj !== 'object') return false;
    if(!target.prototype) throw Error();
-    while(obj){
-      if(Object.getPrototypeOf(obj) == target.prototype){
-        flag = true;
-        break;
-      }
-      obj = Object.getPrototypeOf(obj);
+   if(Object.getPrototypeOf(obj) == target.prototype){
+      return true
     }
-  return flag;
+   return myInstanceOf(Object.getPrototypeOf(obj), target);
 }
