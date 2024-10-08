ca5746a0cfcad44e6627a68bf5de9b3e542a296a
Bidisha Das <officialbidisha1@gmail.com>
2024-10-08 22:53:41 +0530

    Update EventEmitter.js

--- EventEmitter.js
+++ EventEmitter.js
@@ -1,55 +1,37 @@
-// Creating event emitter
-
 class EventEmitter {
   constructor() {
     this.subscriptionList = new Map();
   }
 
+  // Subscribe to an event with a callback function
   subscribe(eventName, callback) {
-    const sym = Symbol();
+    const id = Symbol(); // Unique identifier for the subscription
     if (!this.subscriptionList.has(eventName)) {
-      const callbackMap = new Map();
-      callbackMap.set(sym, callback);
-      this.subscriptionList.set(eventName, callbackMap);
-    } else {
-      const callbackMap = this.subscriptionList.get(eventName);
-      callbackMap.set(sym, callback);
+      this.subscriptionList.set(eventName, new Map());
     }
+    const eventMap = this.subscriptionList.get(eventName);
+    eventMap.set(id, callback);
 
-    const that = this; // store the reference to the EventEmitter instance
+    // Return an object with a release method to unsubscribe
     return {
-      release: function releaseFn() {
-        const list = that.subscriptionList.get(eventName);
-        list && list.delete(sym);
-        // Clean up if there are no more subscriptions for the event
-        if (list && list.size === 0) {
-          that.subscriptionList.delete(eventName);
+      release: () => {
+        // Remove the callback from the event's subscription map
+        if (eventMap.has(id)) {
+          eventMap.delete(id);
+        }
+        // If there are no more subscribers, remove the event entry
+        if (eventMap.size === 0) {
+          this.subscriptionList.delete(eventName);
         }
       },
     };
   }
 
+  // Emit an event with optional arguments
   emit(eventName, ...args) {
-    const subscriptions = this.subscriptionList.get(eventName);
-    if (subscriptions) {
-      subscriptions.forEach((callback) => {
-        callback(...args);
-      });
+    const eventMap = this.subscriptionList.get(eventName);
+    if (eventMap) {
+      eventMap.forEach((cb) => cb.apply(this, args)); // Call each callback with the provided arguments
     }
   }
 }
-
-// Test cases
-
-const emitter = new EventEmitter();
-
-// Test case: release() should work spec
-const sub1 = emitter.subscribe('test', () => console.log('test1'));
-sub1.release();
-emitter.emit('test'); // expects no output
-
-// Test case: release() should work for multiple listeners on the same event spec
-const sub2 = emitter.subscribe('test', () => console.log('test2'));
-const sub3 = emitter.subscribe('test', () => console.log('test3'));
-sub2.release();
-emitter.emit('test'); // expects 'test3' output
