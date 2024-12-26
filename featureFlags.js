77934ce2402132a4b9699c1072f825e743413651
Bidisha Das <officialbidisha1@gmail.com>
2024-12-26 22:04:25 +0530

    Update featureFlags.js

--- featureFlags.js
+++ featureFlags.js
@@ -1,5 +1,3 @@
- * Read FAQs section on the left for more information on how to use the editor
-**/
 const SAMPLE_FEATURES = {
   show_dialog_box: true,
   enable_new_pricing: true,
@@ -8,13 +6,12 @@ const SAMPLE_FEATURES = {
 const Cache = {
   timestamp: null,
   featureFlags: {},
-}
+};
 
 const MAX_CACHE_TTL = 10000;
 
 // returns the state of *all* features for the current user
 function fetchAllFeatures() {
-  // mocking the fetch API call
   console.log('---FETCH ALL FEATURES---');
   return new Promise((resolve) => {
     setTimeout(() => resolve(SAMPLE_FEATURES), 100);
@@ -23,33 +20,37 @@ function fetchAllFeatures() {
 
 // Shared promise for ongoing fetch operation
 let ongoingFetchPromise = null;
+
 // DO NOT CHANGE THE FUNCTION NAME
 function getFeatureState(featureName, defaultValue) {
-  // write your solution here
-
+  // Serve from cache if valid
   if (Cache.featureFlags && Date.now() - Cache.timestamp < MAX_CACHE_TTL) {
-    const value = Object.prototype.hasOwnProperty.call(Cache.featureFlags,
-      featureName) ? Cache.featureFlags[featureName] : defaultValue;
+    const value = Object.prototype.hasOwnProperty.call(Cache.featureFlags, featureName)
+      ? Cache.featureFlags[featureName]
+      : defaultValue;
     return Promise.resolve(value);
   }
 
-  if(ongoingFetchPromise){
-    return ongoingFetchPromise.then((featureFlags)=> {
-      return Object.prototype.hasOwnProperty.call(featureFlags, 
-      featureName)? featureFlags[featureName]: defaultValue;
-    })
+  // Reuse ongoing fetch promise if available
+  if (ongoingFetchPromise) {
+    return ongoingFetchPromise.then((featureFlags) => {
+      return Object.prototype.hasOwnProperty.call(featureFlags, featureName)
+        ? featureFlags[featureName]
+        : defaultValue;
+    });
   }
 
-  ongoingFetchPromise = fetchAllFeatures().then((featureFlags) => {
-    Cache.timestamp = Date.now();
-    Cache.featureFlags = featureFlags;
-    return featureFlags;
-  }).catch((err) => {
-    return err;
-  }).finally(() => {
-    // Always clear the promise after resolution or rejection
-    ongoingFetchPromise = null;
-  });
+  // Initiate a new fetch operation
+  ongoingFetchPromise = fetchAllFeatures()
+    .then((featureFlags) => {
+      Cache.timestamp = Date.now();
+      Cache.featureFlags = featureFlags;
+      return featureFlags;
+    })
+    .finally(() => {
+      // Clear ongoing fetch promise
+      ongoingFetchPromise = null;
+    });
 
   return ongoingFetchPromise.then((featureFlags) => {
     return Object.prototype.hasOwnProperty.call(featureFlags, featureName)
@@ -59,3 +60,22 @@ function getFeatureState(featureName, defaultValue) {
     return defaultValue;
   });
 }
+
+// Example Usage
+getFeatureState("show_dialog_box", false)
+  .then((isEnabled) => {
+    if (isEnabled) {
+      console.log("Show Dialog Box");
+    } else {
+      console.log("Do not show Dialog Box");
+    }
+  });
+
+getFeatureState("enable_new_pricing", false)
+  .then((isEnabled) => {
+    if (isEnabled) {
+      console.log("Enable New Pricing");
+    } else {
+      console.log("Use Old Pricing");
+    }
+  });
