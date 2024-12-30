1142cd2faec014080b39d0ab6ee9c1ae5a41f093
Bidisha Das <officialbidisha1@gmail.com>
2024-12-31 01:43:08 +0530

    Update featureFlags.js

--- featureFlags.js
+++ featureFlags.js
@@ -6,6 +6,47 @@ const SAMPLE_FEATURES = {
 const Cache = {
   timestamp: null,
   featureFlags: {},
+}
+
+const MAX_CACHE_TTL = 10000;
+
+// returns the state of *all* features for the current user
+function fetchAllFeatures() {
+  // mocking the fetch API call
+  console.log('---FETCH ALL FEATURES---');
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(SAMPLE_FEATURES), 100);
+  });
+}
+
+// DO NOT CHANGE THE FUNCTION NAME
+function getFeatureState(featureName, defaultValue) {
+  // write your solution here
+
+  if (Cache.featureFlags && Date.now() - Cache.timestamp < MAX_CACHE_TTL) {
+    const value = Object.prototype.hasOwnProperty.call(Cache.featureFlags,
+      featureName) ? Cache.featureFlags[featureName] : defaultValue;
+    return Promise.resolve(value);
+  }
+  return fetchAllFeatures().then((featureFlags) => {
+    Cache.timestamp = Date.now();
+    Cache.featureFlags = featureFlags;
+    return Object.prototype.hasOwnProperty.call(featureFlags,
+      featureName) ? featureFlags[featureName] : defaultValue;
+  }).catch((err) => {
+    return defaultValue;
+  })
+}
+
+/*
+const SAMPLE_FEATURES = {
+  show_dialog_box: true,
+  enable_new_pricing: true,
+};
+
+const Cache = {
+  timestamp: null,
+  featureFlags: {},
 };
 
 const MAX_CACHE_TTL = 10000;
@@ -79,3 +120,4 @@ getFeatureState("enable_new_pricing", false)
       console.log("Use Old Pricing");
     }
   });
+*/
